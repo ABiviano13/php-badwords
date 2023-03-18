9cfd98aa43fddaa9afbc54161fbc33c07a96334e
ABiviano13 <abiviano0@outlook.com>
2023-03-18 10:15:05 +0100

    Feat form html

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<?php
-
-$name = $_GET['name'];
-
-?>
-
-
-<h1>Ciao <?php echo $name ?> </h1>
+<form action="form.php" method="GET">
+    <textarea name="paragrafo" cols="30" rows="10" placeholder="Scrivi il paragrafo che vuoi stampare"></textarea>
+    <input type="text" placeholder="Parola da censurare" name="censura">
+    <button type="submit">Invia</button>
+</form>
